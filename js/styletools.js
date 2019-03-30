a1f4c8f1c21a84935f7a2375ac0a0ac73270efa3
Argan <argan.piquet@gmail.com>
2019-03-30 12:57:19 +0100

    last commit

--- js/styletools.js
+++ js/styletools.js
@@ -29,6 +29,42 @@
 	var displayYear = copyrightYear.getFullYear();
 		
 	document.getElementsByClassName("copyright")[0].innerHTML = displayYear;
+
+/*** Progress bar ***/
+	(function () {
+		var $bar, perc, start, update;
+		
+		$bar = $('.percentage');
+		perc = 0;
+		
+		update = function () {
+			$bar.css("width", perc + '%');
+			$bar.attr("perc", Math.floor(perc) + '%');
+			perc += 0.2;
+		
+			if (Math.floor(perc) === 5) {
+				$bar.addClass('active')
+			}
+			
+			if (Math.floor(perc) === 95) {
+				$bar.removeClass('active')
+			}
+			
+			if (perc >= 85) {
+				return perc = 85
+			}
+		};
+		
+		start = function () {
+			var run;
+				
+			return run = setInterval(update, 10)
+		};
+
+		start()
+	})
+
+	.call(this)
 	
 /*** Display your age ***/
  	var birthdate = new Date("1997/5/29"); /* Replace by your birthdate ! */
